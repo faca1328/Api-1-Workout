d4c13b6041796b061722e9189874b60dceec1112
Facu M <facundoivanmartinez@hotmail.com>
2024-04-10 10:56:06 -0300

    apicache done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
+const apicache = require('apicache');
 const express = require('express');
 const v1WorkoutRoute = require('./v1/routes/workoutRoute.js');
 
 const app = express();
 const PORT = process.env.PORT || 3001
+const cache = apicache.middleware;
 
 app.use(express.json());
+
+//con esto cacheo por dos minutos (mantengo las cookies para mejor rendimiento por dos minutos)
+app.use(cache('2 minutes'))
 
 // llamamos a la ruta que queremos que se ejecute en esta dirección.
 app.use('/api/v1', v1WorkoutRoute);
